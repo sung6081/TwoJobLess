f530d1016655e294d5e444adcc64887ccf871ce2
sungnam cho <sung6081@gmail.com>
2025-05-20 15:33:25 +0900

    Frist Commit :: 2025/05/20

---
+++ src/main/java/opgg/TwoJobLessApplication.java
@@ -0,0 +1,13 @@
+package opgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoJobLessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoJobLessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/opgg/TwoJobLessApplicationTests.java
@@ -0,0 +1,13 @@
+package opgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoJobLessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
