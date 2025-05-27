6ff95b73c1aff1ac3d2e0a9c75d6ccd5fcf94799
yunsangee <michael0622@naver.com>
2025-05-27 15:24:31 +0900

    05/27 usercontroller에서 view 연동 되는지 확인 완료

--- src/main/java/opgg/User/UserController.java
+++ src/main/java/opgg/User/UserController.java
@@ -1,5 +1,14 @@
 package opgg.User;
 
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173") 
+@RestController
+@RequestMapping("/opgg/user")
 public class UserController {
 
+    @GetMapping("/test")
+    public String test() {
+        return "CORS 연결 성공!";
+    }
 }
