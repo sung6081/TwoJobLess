6f1288b82e28a5e26e21e13a7a7611e912d08d4c
sungnam cho <sung6081@gmail.com>
2025-06-16 11:55:47 +0900

    GoogleRepositoty 수정

--- src/main/java/opgg/repository/GoogleRepository.java
+++ src/main/java/opgg/repository/GoogleRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import opgg.entity.GoogleUser;
 
 
-public interface GoogleRepository extends JpaRepository<GoogleUser, Long> {
+public interface GoogleRepository extends JpaRepository<GoogleUser, String> {
 	Optional<GoogleUser> findByEmail(String email);
 }
